79578b01a54ae9f4eb26827692d550074d9d4566
Patricio Ramonet <patricio.ramonet@skyairline.com>
2021-04-04 22:36:46 -0400

    Added Progess bar on container and webpack prod remotes auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
+
+import Progress from './components/Progress';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 // nombre de clases para CSS, proporcionará aislamiento de estilos para
 // los componentes trabajados dentro del MFE.
@@ -18,10 +21,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <Switch>
-                        <Route path="/auth" component={AuthApp} />
-                        <Route path="/" component={MarketingApp} />
-                    </Switch>
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
